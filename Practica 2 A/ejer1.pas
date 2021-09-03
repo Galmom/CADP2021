5a31e198c12432a3fe757c7b0a72588c616df409
Galmom <83314843+Galmom@users.noreply.github.com>
2021-09-03 19:50:39 -0300

    Create ejer1.pas

---
+++ Practica 2 A/ejer1.pas
@@ -0,0 +1,15 @@
+program alcance1; {IMPRIME 3, 2 Y 1}
+var a,b: integer;
+procedure uno;
+var b: integer;
+begin
+ b := 3;
+ writeln(b);
+end;
+begin
+ a:= 1;
+ b:= 2;
+ uno;
+writeln(b, a);
+readln();
+end.
