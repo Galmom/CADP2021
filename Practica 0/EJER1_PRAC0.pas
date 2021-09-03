1b90bcd5a0775cda06e9d13a5da138fb1349bdd3
Galmom <83314843+Galmom@users.noreply.github.com>
2021-09-03 19:28:23 -0300

    Create EJER1_PRAC0.pas

---
+++ Practica 0/EJER1_PRAC0.pas
@@ -0,0 +1,10 @@
+program ejer1;
+var
+	num1, num2:integer;
+begin
+	writeln('ingrese 2 numeros');
+	readln(num1);
+	readln(num2);
+	writeln('los numeros ingresadaos son: ', num2, ' y ' ,num1);
+	readln();
+end.
