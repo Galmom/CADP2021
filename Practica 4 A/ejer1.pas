46971910e91f18b3da96e242465044f952a5c6fc
Galmom <83314843+Galmom@users.noreply.github.com>
2021-09-17 19:04:30 -0300

    Create ejer1.pas

---
+++ Practica 4 A/ejer1.pas
@@ -0,0 +1,26 @@
+program sumador;
+type
+	vnums = array [1..10] of integer;
+	var
+ 		numeros : vnums;
+ 		i : integer;
+ 	begin
+ 		for i:=1 to 10 do {primer bloque for}
+ 			numeros[i] := i;
+
+	for i := 2 to 10 do {segundo bloque for}
+		numeros[i] := numeros[i] + numeros [i-1]
+	end.
+
+{a) ¿Qué valores toma la variable numeros al finalizar el primer bloque for?
+	10
+b) Al terminar el programa, ¿con qué valores finaliza la variable números?
+	47
+c) Si se desea cambiar la línea 11 por la sentencia: for i:=1 to 9 do ¿Cómo debe modificarse el
+código para que la variable números contenga los mismos valores que en 1.b)?
+	numeros[i+1] := numeros[i+1] + numeros [i];
+d) ¿Qué valores están contenidos en la variable numeros si la líneas 11 y 12 se reemplazan por:
+for i:=1 to 9 do
+numeros[i+1] := numeros[i];
+	111111111
+}
